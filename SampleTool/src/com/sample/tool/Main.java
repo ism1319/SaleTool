fbb04713fee40130845334a6d4bdaee7513b8215
Iqbal <iqbal_1319@rediffmail.com>
2014-12-14 22:43:58 +0530

    Initial checkin the sample tool.

---
+++ SampleTool/src/com/sample/tool/Main.java
@@ -0,0 +1,16 @@
+package com.sample.tool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SampleDisplay data = new SampleDisplay();
+		displayMessage();
+		data.displayData();
+	}
+
+	public static void displayMessage(){
+		System.out.println("INSIDE DISPLAY MESSAGE");
+		System.out.println("INSIDE GIT DEMO");
+	}
+}
---
+++ SampleTool/src/com/sample/tool/SampleDisplay.java
@@ -0,0 +1,12 @@
+package com.sample.tool;
+
+public class SampleDisplay {
+
+	
+	int data[]={5,7,9};
+	
+	public void displayData(){
+		
+		System.out.println("Inside displayData");
+	}
+}
