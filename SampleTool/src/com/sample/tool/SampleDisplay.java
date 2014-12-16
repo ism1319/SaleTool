f9d8bd14ad33ee0f19588aa46dc03dd363d92aac
ism1319 <iqbal_1319@rediffmail.com>
2014-12-16 12:48:34 +0530

    Added the new for loop and summed data

--- SampleTool/src/com/sample/tool/SampleDisplay.java
+++ SampleTool/src/com/sample/tool/SampleDisplay.java
@@ -1,15 +1,21 @@
 package com.sample.tool;
 
+import java.util.Iterator;
+
 public class SampleDisplay {
 
 	
-	int data[]={5,7,9};
+	int data[]={1,5,7,9,15};
 	
 	public void displayData(){
 		
+		int sum =0;
 		System.out.println("Inside displayData");
-		for(int i=0;i<data.length;i++){
+		for (int i : data) {
 			System.out.println("data!!!: "+data[i]);
+			sum+=data[i];
 		}
+		
+		System.out.println("Summed number :"+sum);
 	}
 }
