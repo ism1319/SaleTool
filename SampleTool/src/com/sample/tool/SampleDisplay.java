69b4715b6e4013a73217367bb93e98090845c581
ism1319 <iqbal_1319@rediffmail.com>
2014-12-16 12:48:05 +0530

    Summed the sales data

--- SampleTool/src/com/sample/tool/SampleDisplay.java
+++ SampleTool/src/com/sample/tool/SampleDisplay.java
@@ -1,15 +1,22 @@
 package com.sample.tool;
 
+/*
+ * This Sample data is summed the sales data.
+ */
 public class SampleDisplay {
 
 	
 	int data[]={5,7,9};
 	
 	public void displayData(){
-		
+		int sum=0;
 		System.out.println("Inside displayData");
+		
 		for(int i=0;i<data.length;i++){
 			System.out.println("data!!!: "+data[i]);
+			sum+=data[i];
 		}
+		
+		System.out.println("Sum sales data :"+sum);
 	}
 }
